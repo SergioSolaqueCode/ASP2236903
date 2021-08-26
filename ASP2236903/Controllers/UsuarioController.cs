1220bfce7dd98a88ab49a62afd53fd86b82c7bb6
SergioSolaqueCode <sergiorealg3@gmail.com>
2021-08-26 09:13:20 -0500

    porbando metodos detalles, eliminar y editar

---
+++ ASP2236903/Controllers/UsuarioController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.usuario.Find(id);
+                return View(findUser);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.usuario.Find(id);
+                    db.usuario.Remove(findUser);
+                    return RedirectToAction("index");
+                }
+            } catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+           
+            
+                
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View();
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(usuario editUser)
+        {
+           
+            try
+            { 
+                using (var db = new inventario2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+                    return RedirectToAction("Index)");
+                }
+
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
