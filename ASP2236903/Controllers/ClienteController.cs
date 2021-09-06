203ea344a2c13049f5c3fb141a4a00ea3a2a36c9
SergioSolaqueCode <sergiorealg3@gmail.com>
2021-09-06 10:05:57 -0500

    Crud cliente

---
+++ ASP2236903/Controllers/ClienteController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findcliente = db.cliente.Find(id);
+                return View(findcliente);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findCliente = db.cliente.Find(id);
+                    db.cliente.Remove(findCliente);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente findCliente = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findCliente);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(cliente editCliente)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+
+                    cliente cliente = db.cliente.Find(editCliente.id);
+                    cliente.nombre = editCliente.nombre;
+                    cliente.documento = editCliente.documento;
+                    cliente.email = editCliente.email;
+                    return RedirectToAction("Index)");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+    }
+}
