51596a608861d70c20dbd2cf2cdce86b05192200
SergioSolaqueCode <sergiorealg3@gmail.com>
2021-09-22 13:32:33 -0500

    falta hacer la vista parcial de usuario en usuaraio rol

---
+++ ASP2236903/Controllers/UsuarioRolController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class UsuarioRolController : Controller
+    {
+        // GET: UsuarioRol
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+        }
+
+        public static String NombreUsuario(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public static String DescripcionRol(int idRol)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.roles.Find(idRol).descripcion;
+            }
+        }
+
+        public ActionResult ListarUsuarios()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult ListarRoles()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.roles.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(usuariorol usuariorol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuariorol.Add(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol findUsuarioRol = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUsuarioRol);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(usuariorol editUsuarioRol)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol usuariorol = db.usuariorol.Find(editUsuarioRol.id);
+
+                    usuariorol.idUsuario = editUsuarioRol.idUsuario;
+                    usuariorol.idRol = editUsuarioRol.idRol;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol usuariorol = db.usuariorol.Find(id);
+                    db.usuariorol.Remove(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
