3d6fdae19070984926d5067ca301024ea9331a7c
SergioSolaqueCode <sergiorealg3@gmail.com>
2021-10-03 14:48:30 -0500

    modelo custom y vista custom

--- ASP2236903/Controllers/ProductoController.cs
+++ ASP2236903/Controllers/ProductoController.cs
@@ -133,6 +133,30 @@ namespace ASP2236903.Controllers
                 return View();
             }
         }
+        public ActionResult ReporteMCustom()
+        {
+            try
+            {
+                var db = new inventario2021Entities();
+                var query = from tabProveedor in db.proveedor
+                            join tabProducto in db.producto on tabProveedor.id equals tabProducto.id_proveedor
+                            select new Reporte
+                            {
+                                nombreProveedor = tabProveedor.nombre,
+                                telefonoProveedor = tabProveedor.telefono,
+                                direccionProveedor = tabProveedor.direccion,
+                                nombreProducto = tabProducto.nombre,
+                                precioProducto = tabProducto.percio_unitario
+                            };
+                return View(query);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
 
 
 
