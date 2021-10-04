413a168e3129a7dea89d2b761480b3ae137d5b7e
SergioSolaqueCode <sergiorealg3@gmail.com>
2021-10-04 11:07:27 -0500

    cargue masivo pero no encuentra la vista

--- ASP2236903/Controllers/ProveedorController.cs
+++ ASP2236903/Controllers/ProveedorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ASP2236903.Models;
+using System.IO;
 
 namespace ASP2236903.Controllers
 {
@@ -123,5 +124,68 @@ namespace ASP2236903.Controllers
             }
 
         }
+        [HttpPost]
+        public ActionResult CargueMasivo(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                //string para guardar la ruta
+                string filePath = string.Empty;
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    //guardar el archivo
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newProveedor = new proveedor
+                            {  
+                                nombre = row.Split(';')[0],
+                                direccion = row.Split(';')[1],
+                                telefono = row.Split(';')[2],
+                                nombre_contacto = row.Split(';')[3]
+                            };
+
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.proveedor.Add(newProveedor);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
     }
 }
